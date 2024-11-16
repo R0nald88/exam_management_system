512ead7dc7078eef475e9d6dc0d32ed632212b69
Ronald <tkcheungan@ust.hk>
2024-11-16 16:41:54 +0800

    Finished Teacher Register and Login UI

---
+++ src/main/java/comp3111/examsystem/entity/Personnel/Gender.java
@@ -0,0 +1,26 @@
+package comp3111.examsystem.entity.Personnel;
+
+import java.io.Serializable;
+
+public enum Gender implements Serializable {
+    MALE("Male"),
+    FEMALE("Female"),
+    NON_BINARY("Non-binary");
+
+    private final String name;
+    Gender(String s) {
+        name = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Gender toGender(String s) {
+        for (Gender g : Gender.values()) {
+            if (g.name.equals(s)) return g;
+        }
+
+        return Gender.valueOf(s);
+    }
+}
