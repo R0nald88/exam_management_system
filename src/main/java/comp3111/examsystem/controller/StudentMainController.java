d8959d85ec44d04a31aa290d4b049f1b662f0e36
Marcusx1i <lichingho@gmail.com>
2024-11-16 08:14:21 +0800

    Finish drafting all UIs. Finish part of the Student's controller classes and identify TODO for remaining work

--- src/main/java/comp3111/examsystem/controller/StudentMainController.java
+++ src/main/java/comp3111/examsystem/controller/StudentMainController.java
@@ -1,14 +1,19 @@
 package comp3111.examsystem.controller;
 
 import comp3111.examsystem.Main;
+import comp3111.examsystem.entity.Exam.Exam;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,11 +26,39 @@ public class StudentMainController implements Initializable {
     }
 
     @FXML
-    public void openExamUI() {
+    public void openExamUI(ActionEvent e) {
+        Exam selectedExam = null;// Retrieve selected exam details
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentStartExamUI.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Start Exam");
+        try {
+            Parent root = fxmlLoader.load();
+            StudentStartExamController studentStartExamController = fxmlLoader.getController();
+            studentStartExamController.setExam(selectedExam);
+            stage.setScene(new Scene(fxmlLoader.load()));
+        } catch ( IOException e1) {
+            e1.printStackTrace();
+        }
+        stage.show();
+        ((Stage) ((Button) e.getSource()).getScene().getWindow()).close();
     }
 
     @FXML
-    public void openGradeStatistic() {
+    public void openGradeStatistic(ActionEvent e) {
+        //TODO: set Student Grade Statistics content to this student
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("StudentGradeStatisticUI.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Grade Statistics");
+        try {
+            stage.setScene(new Scene(fxmlLoader.load()));
+        } catch ( IOException e1) {
+            e1.printStackTrace();
+        }
+        stage.show();
+        ((Stage) ((Button) e.getSource()).getScene().getWindow()).close();
     }
 
     @FXML
