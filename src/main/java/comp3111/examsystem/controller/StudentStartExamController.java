114fc1f888fac147729243fd5afca48c47bddefc
Marcusx1i <lichingho@gmail.com>
2024-11-20 00:46:40 +0800

    Add full score to submission

--- src/main/java/comp3111/examsystem/controller/StudentStartExamController.java
+++ src/main/java/comp3111/examsystem/controller/StudentStartExamController.java
@@ -448,7 +448,7 @@ public class StudentStartExamController implements Initializable {
             SubmissionDatabase.getInstance().addSubmission(submission);
             MsgSender.showConfirm("Hint", "Student submit exam successful.", () -> close(e));
             MsgSender.showConfirm("Your Exam Score", submission.getNumberOfCorrect() + "/" + exam.getQuestionIds().size() +" Correct, the precision is "
-                    + (int) (((double) submission.getScore()/exam.getFullScore()) * 100) + "%, the score is " + submission.getScore() + "/" + exam.getFullScore(), () -> { });
+                    + (int) (((double) submission.getScore()/submission.getFullScore()) * 100) + "%, the score is " + submission.getScore() + "/" + submission.getFullScore(), () -> { });
         } catch (Exception e1) {
             MsgSender.showConfirm("Submit Error", e1.getMessage(), () -> {
             });
